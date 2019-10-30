8afbb3b1a51432e2944d6be2d87619596244f553
Jack Dwyer <jdwy215@gmail.com>
2019-10-29 22:45:13 -0400

    :adding

--- app/src/main/java/com/foursquare/jdwyer/buttoncounterapp/MainActivity.kt
+++ app/src/main/java/com/foursquare/jdwyer/buttoncounterapp/MainActivity.kt
@@ -2,8 +2,11 @@ package com.foursquare.jdwyer.buttoncounterapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private var userInput: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
