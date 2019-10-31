9f29fe27858ff74a788129d09c703bc3ce7c1103
Jack Dwyer <jdwy215@gmail.com>
2019-10-30 23:21:01 -0400

    adding solution

--- app/src/main/java/com/foursquare/jdwyer/buttoncounterapp/MainActivity.kt
+++ app/src/main/java/com/foursquare/jdwyer/buttoncounterapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.foursquare.jdwyer.buttoncounterapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -21,11 +22,13 @@ class MainActivity : AppCompatActivity() {
         userInput = findViewById(R.id.editText)
         button = findViewById(R.id.btn)
         textView = findViewById(R.id.textView2)
+        textView?.text = ""
+        textView?.movementMethod = ScrollingMovementMethod()
 
         button?.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 numTimesClicked++
-                textView?.append("\n the button has been clicked $numTimesClicked times thus far")
+                textView?.append("the button has been clicked $numTimesClicked times thus far\n")
             }
         })
     }
