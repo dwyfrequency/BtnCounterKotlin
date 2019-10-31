43d83b09f064debf1d51b734f66386cd032882a9
Jack Dwyer <jdwy215@gmail.com>
2019-10-30 23:11:27 -0400

    adding click listener

--- app/src/main/java/com/foursquare/jdwyer/buttoncounterapp/MainActivity.kt
+++ app/src/main/java/com/foursquare/jdwyer/buttoncounterapp/MainActivity.kt
@@ -2,14 +2,31 @@ package com.foursquare.jdwyer.buttoncounterapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     private var userInput: EditText? = null
+    private var button: Button? = null
+    private var textView: TextView? = null
+    private var numTimesClicked = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userInput = findViewById(R.id.editText)
+        button = findViewById(R.id.btn)
+        textView = findViewById(R.id.textView2)
+
+        button?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                numTimesClicked++
+                textView?.append("\n the button has been clicked $numTimesClicked times thus far")
+            }
+        })
     }
 }
